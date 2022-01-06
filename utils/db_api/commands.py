ebd31909e070d35f9f94c1001e97028c80497dfc
bichief <89649583+bichief@users.noreply.github.com>
2022-01-06 20:45:32 +0300

    update 0.4

--- utils/db_api/commands.py
+++ utils/db_api/commands.py
@@ -4,7 +4,7 @@ from utils.db_api.base import get_session
 from utils.db_api.schemas.user import User
 
 
-async def add_user(telegram_id, username, session: AsyncSession):
+async def add_user(telegram_id, username, session: AsyncSession = get_session()):
     user = User(telegram_id=telegram_id, username=username)
     session.add(user)
     await session.commit()
