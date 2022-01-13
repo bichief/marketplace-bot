94189372b8e2632fa5d62aa5610983b8fbbf6c36
bichief <89649583+bichief@users.noreply.github.com>
2022-01-13 20:02:23 +0300

    Update support_link.py

--- keyboards/inline/support_link.py
+++ keyboards/inline/support_link.py
@@ -2,6 +2,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 support_link = InlineKeyboardMarkup(row_width=1, inline_keyboard=[
     [
-        InlineKeyboardButton(text='Агент №1', url='t.me/ugly2speak')
+        InlineKeyboardButton(text='Агент №1', url='t.me/ungly2speak')
     ]
 ])
